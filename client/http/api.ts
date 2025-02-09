daedc6a8d0c824bdfc4eeeaa7e87e84cb178693b
abdullohqurbon0v <abdullohqurbonov332@gmail.com>
2025-02-09 03:38:31 -0500

    fix api error

--- client/http/api.ts
+++ client/http/api.ts
@@ -1,7 +1,7 @@
 import axios from 'axios'
 
 export const fetchData = axios.create({
-  baseURL: "https://apimyhomegroup.onrender.com/",
+  baseURL: "https://apimyhomegroup.onrender.com/api/",
   timeout: 10000,
   headers: {
     'Content-Type': 'application/json',
